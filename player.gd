40c162b1840ce2f5c81bd9fa0a1e0537959eab3b
evezuu <shreyatodi3@gmail.com>
2026-04-22 00:07:46 -0400

    things-added

--- player.gd
+++ player.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+signal hit
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -11,15 +11,26 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_killzone_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+func die():
+	hit.emit()
+	queue_free()
+	
+func _on_mob_detector_body_entered(body):
+	die()
